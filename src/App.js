6b9218d1cdae8fe0fd1e543f7e8ff8ef969868da
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-03-30 00:29:31 +0200

    Create route for SingleProduct

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import SingleProduct from "./pages/SingleProduct";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/product/:productId" element={<SingleProduct />}/>
         <Route path="/cart" element={<Cart />} />
       </Routes>
      </Router>
