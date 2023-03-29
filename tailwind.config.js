dd30221f55bd487f17fd6da7f1ba9ef46be94e65
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-03-30 00:29:11 +0200

    customize Tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'small': '440px',
+      // => @media (min-width: 640px) { ... }
+
+      'medium': '824px',
+      // => @media (min-width: 1024px) { ... }
+
+      'large': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
