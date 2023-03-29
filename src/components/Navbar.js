519f5587de6fb70844e6594393a12db5c4d74b21
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-03-29 23:22:45 +0200

    Basic navigation and add Home and Cart pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='bg-gray-800 h-16 text-white'>
+      <div className='sm:container px-6 h-16 mx-auto flex justify-between items-center'>
+        <a href="">Logo</a>
+        <nav className='nav'>
+          <ul className='flex flex-row space-x-4'>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/cart">Cart</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
