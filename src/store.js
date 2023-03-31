e7bafbd0713efb194effb1c282971b6ba3825a3a
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-04-01 01:46:42 +0200

    minor changes

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import CartSlice from './reducers/CartReducer'
+import {CartSlice} from './reducers/CartReducer'
 export const store = configureStore({
   reducer: {
     cart: CartSlice
