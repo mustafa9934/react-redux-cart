4179cb3e37fa75747d75450ec290fbbd45e56f61
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-04-01 01:48:21 +0200

    update CartSlice

--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -1,17 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { ADD_TOCART } from '../actionTypes/AddToCartTypes'
 
 const initialState = {
-  items: []
+  items: [],
+  totalQuantity: 0
 }
 
-export const CartSlice = createSlice({
-  name: 'cart',
-  initialState,
-  reducers: {
-  },
-})
-
-// Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = CartSlice.actions
-
-export default CartSlice.reducer
+export const CartSlice = (state= initialState, action) => {
+  switch(action.type){
+    case ADD_TOCART:
+      const product = action.payload;
+      const checkProduct = state.items.find(item => item.id === product.id)
+     if (checkProduct) {
+        checkProduct.price += action.payload.price;
+        checkProduct.quantity ++;
+        state.totalQuantity ++;
+     } else {
+      state.items.push({
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        totalPrice: product.price,
+        quantity: 1
+      })
+      state.totalQuantity ++;
+     }
+      return {...state}
+    default:
+      return state;
+  }
+}
