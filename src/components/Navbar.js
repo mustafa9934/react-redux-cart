23558a72b76edeaa18e9050ee4144a615f3d4b9d
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-04-03 03:35:59 +0200

    display total quantity in cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const {totalQuantity} = useSelector(state => state.cart)
   return (
     <div className='bg-gray-800 h-16 text-white'>
       <div className='container px-6 h-16 mx-auto flex justify-between items-center'>
@@ -9,7 +11,7 @@ const Navbar = () => {
         <nav className='nav'>
           <ul className='flex flex-row space-x-4'>
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
+            <li><Link to="/cart">Cart</Link> <span className='bg-gray-300 text-gray-900 py-1 px-2 rounded-full'>{totalQuantity}</span> </li>
           </ul>
         </nav>
       </div>
