eb71e00080ca9095fefa21fa8ee7a4cf091f259c
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-04-03 03:53:47 +0200

    increase and decrease methods in cartReducer

--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TOCART } from '../actionTypes/AddToCartTypes'
+import { ADD_TOCART, DECREASE_ITEM, INCREASE_ITEM } from '../actionTypes/AddToCartTypes'
 
 const initialState = {
   items: [],
@@ -18,12 +18,30 @@ export const CartSlice = (state= initialState, action) => {
       state.items.push({
         id: product.id,
         title: product.title,
+        description: product.description,
         price: product.price,
         totalPrice: product.price,
-        quantity: 1
+        quantity: 1,
+        image: product.image
       })
       state.totalQuantity ++;
      }
+      return {...state}
+    case DECREASE_ITEM:
+      let checkItem = state.items.find(item => item.id === action.payload.id)
+      if (checkItem && checkItem.quantity > 0) {
+        checkItem.quantity --;
+        checkItem.totalPrice -= action.payload.price;
+        state.totalQuantity --;
+      }
+      return {...state}
+    case INCREASE_ITEM:
+      let checkItem1 = state.items.find(item => item.id === action.payload.id)
+      if (checkItem1) {
+        checkItem1.quantity ++;
+        checkItem1.totalPrice += action.payload.price;
+        state.totalQuantity ++;
+      }
       return {...state}
     default:
       return state;
